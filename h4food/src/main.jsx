3e1c5db99ea38b70e75b02a6b7f41d1b597bef18
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-11-05 00:26:27 +0600

    installed tailwind and basic router setup completed

--- h4food/src/main.jsx
+++ h4food/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root";
+import Home from "./components/pages/Home/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ h4food/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF007F",
+        "text-primary": "#151515",
+        "text-secondary": "#444",
+        "text-light": "#737373",
+      },
+    },
+    fontFamily: {
+      primary: ["Poppins", "sans-serif"],
+      secondary: ["Yanone Kaffeesatz", "serif"],
+    },
+  },
+  plugins: [],
+};
