ca17bdcb87dea7a2f8846c43807f0894977e52c3
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-11-06 21:11:55 +0600

    search functionality added in all food items page

--- h4food/src/pages/AllFoodItems/AllFoodItems.jsx
+++ h4food/src/pages/AllFoodItems/AllFoodItems.jsx
@@ -3,22 +3,88 @@ import { useEffect, useState } from "react";
 import SingleFood from "./SingleFood";
 
 export default function AllFoodItems() {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+    });
+  }, []);
+
   const [allFoods, setAllFoods] = useState([]);
+  const [filteredFoods, setFilteredFoods] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
-    axios("http://localhost:5000/api/v1/foodItems").then((res) =>
-      setAllFoods(res.data)
-    );
+    axios.get("http://localhost:5000/api/v1/foodItems").then((res) => {
+      setAllFoods(res.data);
+      setFilteredFoods(res.data);
+    });
   }, []);
+
+  const handleClear = () => {
+    setFilteredFoods(allFoods);
+    setSearchText("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const search = e.target.search.value;
+
+    const filter = allFoods.filter((food) => {
+      return food.foodName == search;
+      // return food.foodName.indexOf(search) != -1;
+    });
+    setFilteredFoods(filter);
+  };
 
   return (
     <div className="mb-20">
-      <h2 className="text-5xl font-semibold text-center text-primary my-10">All Food Items</h2>
+      <h2 className="text-5xl font-semibold text-center text-primary my-10">
+        All Food Items
+      </h2>
+      <div className="mb-10 text-center">
+        <form
+          onSubmit={handleSubmit}
+          className="mx-auto text-center flex flex-row items-center justify-center "
+        >
+          <input
+            onChange={(e) => setSearchText(e.target.value)}
+            required
+            type="text"
+            name="search"
+            value={searchText}
+            className="border outline-none rounded-l-lg w-1/4 bg-rose-100 px-5 py-3"
+          />
+          <button className="bg-primary px-5 py-[0.7rem] rounded-r-lg text-xl font-medium text-white">
+            Search
+          </button>
+        </form>
+        <button
+          onClick={handleClear}
+          className="border-2 border-primary px-10 mt-5 py-[0.7rem] rounded-lg text-lf font-medium "
+        >
+          Clear Search
+        </button>
+      </div>
+
       <div className="grid grid-cols-1 lg:grid-cols-2 mx-60 gap-10">
-        {allFoods.map((food) => (
+        {filteredFoods.map((food) => (
           <SingleFood key={food._id} food={food} />
         ))}
+        {/* {allFoods.map((food) => (
+          <SingleFood key={food._id} food={food} />
+        ))} */}
       </div>
+      {filteredFoods.length == 0 ? (
+        <div className="h-[40vh] flex justify-center items-center">
+          {/* <h2 className="text-5xl text-center  text-primary font-semibold">
+            Sorry no item available
+          </h2> */}
+          <span className="loading loading-spinner loading-lg text-primary"></span>
+          ;
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
